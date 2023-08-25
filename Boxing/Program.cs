f6264fb4f6a078a14682bcf81008d433fd863ab8
Kulpot <kulpotwahu@gmail.com>
2023-08-25 11:56:33 +0800

    Add project files.

---
+++ Boxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=Iod4m852YqQ&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=10
+//  boxing -- downside garbage collection - heap defragmentation - 
+
+class MainClass
+{
+    static void Main()
+    {
+        //int i = 5;
+        //object o = i;
+        //i = 20;
+        //Console.WriteLine(o);
+        //Console.WriteLine(i);
+        //o = 99;
+        //Console.WriteLine(o);
+
+        ////////
+
+        int i = 5;
+        object o = i;
+        i++;
+    }
+}
